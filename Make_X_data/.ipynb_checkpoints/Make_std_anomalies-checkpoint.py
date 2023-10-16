302834dbda93b5633fbb771e66b9778e13fb13b0
rlbaiman <rebecca.baiman@colorado.edu>
2023-10-16 13:24:14 -0600

    update readme

---
+++ Make_X_data/.ipynb_checkpoints/Make_std_anomalies-checkpoint.py
@@ -0,0 +1,71 @@
+# Import necessary libraries
+import numpy as np
+import xarray as xr
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+
+variable_index = int(sys.argv[1])
+
+# Functions
+def make_IWV_climo_stats(data_input):
+    """
+    input data and create a symmetric distribution based on the right hand side of the distribution. 
+    Use this for IWV before calculating the std and mean so that the std is not skewed by the limit
+    of the distribution at 0. 
+    """
+    mean = data_input.mean(dim = 'time')
+    data1 = data.where(data>= mean)
+    data2 = mean - np.abs(data1 - mean)
+    data2['time'] = data2.time +pd.Timedelta('1H')
+    
+    data_updated = xr.concat((data1, data2), dim = 'time')
+    
+    mean_out = data_updated.groupby("time.month").mean('time')
+    std_out = data_updated.groupby("time.month").std('time')
+    return(mean_out, std_out)
+
+
+fp_out_3 = '/rc_scratch/reba1583/variable_yr_files_3/'
+variables = [
+    'U',
+    'V',
+    'T',
+    'SLP',
+    'EFLUX',
+    'LWTNET',
+    'sf',
+    'IWV',
+]
+variable = variables[variable_index]
+
+
+data = xr.open_mfdataset(fp_out_3+variable+'*').load()
+if 'lev' in data.dims:
+    data = data.squeeze()
+    data = data.drop('lev')
+
+#add uniform lat_index
+lat_index = np.arange(0,32)
+data = data.assign_coords(lat_index=("lat", lat_index))
+data = data.swap_dims({'lat':'lat_index'})
+    
+if variable == 'LWTNET': # LWTNET is binary so it does not need to be normalized
+    data.to_netcdf('/rc_scratch/reba1583/variable_yr_files_4/'+variable)
+else:
+    if variable =='IWV':
+        # base standard deviation off of right half of IWV distribution
+        climo_mean, climo_std = make_IWV_climo_stats(data) 
+    else:
+        climo_mean = data.groupby("time.month").mean('time')
+        climo_std = data.groupby("time.month").std('time')
+
+    stand_anomalies = xr.apply_ufunc(
+        lambda x, m, s: (x - m) / s,
+        data.groupby("time.month"),
+        climo_mean,
+        climo_std,
+    )
+    stand_anomalies = stand_anomalies.drop('month')
+    stand_anomalies.to_netcdf('/rc_scratch/reba1583/variable_yr_files_4/'+variable)
---
+++ Make_X_data/.ipynb_checkpoints/Make_yrly_data-checkpoint.py
@@ -0,0 +1,136 @@
+# Import necessary libraries
+import numpy as np
+import xarray as xr
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+import os
+
+year = 1980+int(sys.argv[1])
+
+
+def make_LWTNET_binary(lwtnet_data):
+    """
+    LWTNET is labeled 0 (no convection) or 2(strong tropical convection).
+    We use 2 instead of 1 so it is more comparable to normalized anomaly data
+    """
+    lwtnet_data = xr.where(lwtnet_data<220,2,0)
+    return(lwtnet_data)
+
+def resample(file_name, index): 
+    """
+    Function to open the variable file at a single level and resample according training needs.
+    This includes smoothing in time, lat, and lon. Also includes making LWTNET binary and 
+    shifting variables to selected variable_leadtimes
+    """
+    data =  xr.open_mfdataset(fp_out_2+file_name)
+    
+    if variables[i] == 'sf':
+        data = data.reindex(lat=list(reversed(data.lat)))
+        
+    data = data.resample(time = '6H').mean()
+    
+    if variable_leadtimes[index] !=0:
+        data = data.shift(time = int(variable_leadtimes[index]/6), fill_value = np.nan)
+    
+    data = data.interp(lon = np.linspace(-180,180,256), lat = np.linspace(variable_lats[index][0], variable_lats[index][1], 32))
+            
+    if variables[i] == 'LWTNET':
+        data = xr.where(data<220,3,0)
+ 
+    data.to_netcdf(fp_out_3+file_name+'.nc')
+
+
+
+fp = '/pl/active/ATOC_SynopticMet/data/ar_data/Research2/3hrly_merra2_hemisphere/'
+fp_out_1 = '/rc_scratch/reba1583/variable_yr_files/'
+fp_out_2 = '/rc_scratch/reba1583/variable_yr_files_2/'
+fp_out_3 = '/rc_scratch/reba1583/variable_yr_files_3/'
+
+variables = [
+    'U',
+    'V',
+    'T',
+    'SLP',
+    'EFLUX',
+    'LWTNET',
+    'sf',
+    'IWV',
+]
+variable_files = [
+    fp+str(year)+'*',
+    fp+str(year)+'*',
+    fp+str(year)+'*',
+    fp+str(year)+'*',
+    fp+'EFLUX/EFLUX_'+str(year)+'*',
+    fp+'LWTNET/LWTNET_'+str(year)+'*',
+    fp+'200streamfunc/sf_'+str(year)+'*',
+    fp+'IWV/'+str(year)+'*',
+    
+]
+
+variable_levels = [
+    '950',
+    '950',
+    '950',
+    None,
+    None,
+    None,
+    None, 
+    None,
+]
+
+variable_lats = [
+    [-75,-45],
+    [-75,-45],
+    [-80,-50],
+    [-75,-45],
+    [-50,-20],
+    [-30,0],
+    [-90,0],
+    [-70,-40]
+]
+
+# time before that you would like to select (in hours)
+# note: positive values means we are looking at _hours before AR landfall
+variable_leadtimes = [
+    0,0,0,0,0,96,48,0
+]
+
+
+
+
+for i in range(len(variables)):
+    variable = variables[i]
+    file_name = variable+'_'+str(year)
+    
+    
+    if os.path.exists(fp_out_3+file_name+'.nc'):
+        print(file_name+' already processed')
+    
+    else:
+        print('creating '+file_name)  
+
+        # select the variable
+        command = 'cdo -select,name='+variable+' '+variable_files[i]+' '+fp_out_1+variable+'_'+str(year)
+        os.system(command)
+
+        # select the level if necessary
+        if variable_levels[i] is None:
+            command_2 = 'cp '+fp_out_1+variable+'_'+str(year)+' '+fp_out_2+variable+'_'+str(year)
+            os.system(command_2)
+
+        else:
+            command_3 = 'cdo -sellevel,'+variable_levels[i]+' '+fp_out_1+variable+'_'+str(year)+' '+fp_out_2+variable+'_'+str(year)
+            os.system(command_3)
+
+        print(variable+'_'+str(year)+' in variable_yr_files_2')
+
+
+
+        resample(file_name, i)
+        print(file_name+' in variable_yr_files_3')
+
+
+        os.system('rm '+fp_out_1+file_name)
+        os.system('rm '+fp_out_2+file_name)
