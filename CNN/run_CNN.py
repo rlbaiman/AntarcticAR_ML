770d7b9951eeb5189f2f919a353f2c241e0fb440
rlbaiman <rebecca.baiman@colorado.edu>
2024-03-01 15:35:37 -0700

    updates for cnn small testing

--- CNN/run_CNN.py
+++ CNN/run_CNN.py
@@ -12,11 +12,11 @@ from gewitter_functions import get_acc
 
 ## Inputs
 batch_size = 64
-epoch_num = 10
+epoch_num = 100
 
 
-ds_train = xr.open_dataset('/rc_scratch/reba1583/CNN_data_limitNoAR/train.nc')
-ds_val = xr.open_dataset('/rc_scratch/reba1583/CNN_data_limitNoAR/validate.nc')
+ds_train = xr.open_dataset('/rc_scratch/reba1583/CNN_data_multiAR/train.nc')
+ds_val = xr.open_dataset('/rc_scratch/reba1583/CNN_data_multiAR/validate.nc')
 
 train_random_shuffle = np.arange(len(ds_train.features))
 np.random.shuffle(train_random_shuffle )
@@ -31,8 +31,8 @@ X_val = ds_val.features.values[val_random_shuffle]
 Y_val = ds_val.labels_1d.values[val_random_shuffle]
 time_val = ds_val.time[val_random_shuffle]
 
-train_data = tf.data.Dataset.from_tensor_slices((X_train, utils.to_categorical(Y_train)))
-val_data = tf.data.Dataset.from_tensor_slices((X_val, utils.to_categorical(Y_val)))
+train_data = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
+val_data = tf.data.Dataset.from_tensor_slices((X_val, Y_val))
 
 #batch both 
 train_data = train_data.batch(batch_size)
@@ -89,7 +89,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(16,activation='relu'),
     tf.keras.layers.Dropout(0.3),  # Add dropout layer
     #output layer 
-    tf.keras.layers.Dense(11,activation='softmax'),
+    tf.keras.layers.Dense(11,activation='sigmoid'),
 ])
 
 model.summary()
@@ -104,20 +104,15 @@ y_preds_distribution = model.predict(val_data)
 
 
 results_pd = pd.DataFrame(y_preds_distribution)
-results_pd.insert(0,'predY_3',np.zeros((len(results_pd))))
-results_pd.insert(0,'predY_2',np.zeros((len(results_pd))))
-results_pd.insert(0,'predY_1',np.zeros((len(results_pd))))
-results_pd.insert(0,'trueY',Y_val)
-results_pd.insert(0,'timestep',np.array(time_val))
+results_pd = results_pd.set_index(np.array(time_val))
+
+val_pd = pd.DataFrame(Y_val)
+val_pd = val_pd.set_index(np.array(time_val))
 
 
-for n in range(len(results_pd)):
-    results_pd.at[n , 'predY_1'] = int(np.argwhere(np.array(results_pd.iloc[n, 5:16]) == np.sort(results_pd.iloc[n, 5:16])[-1]))
-    results_pd.at[n , 'predY_2'] = int(np.argwhere(np.array(results_pd.iloc[n, 5:16]) == np.sort(results_pd.iloc[n, 5:16])[-2]))
-    results_pd.at[n , 'predY_3'] = int(np.argwhere(np.array(results_pd.iloc[n, 5:16]) == np.sort(results_pd.iloc[n, 5:16])[-3]))
-
-new_folder = '/rc_scratch/reba1583/CNN_test2'
+new_folder = '/rc_scratch/reba1583/CNN_test3'
 os.system('mkdir '+new_folder)
 results_pd.to_csv(new_folder+'/results.csv')
+val_pd.to_csv(new_folder+'/val.csv')
 model.save(new_folder+'/model.keras')
 history_pd.to_csv(new_folder+'/history.csv',index= False)
