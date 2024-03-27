88ba04a8acf229394e73a22ab29673fb1254086a
rlbaiman <rebecca.baiman@colorado.edu>
2024-03-27 14:32:58 -0600

    CNN_daily_test3_fulldata

--- CNN/run_CNN.py
+++ CNN/run_CNN.py
@@ -12,23 +12,23 @@ from gewitter_functions import get_acc
 
 ## Inputs
 batch_size = 32
-epoch_num = 150
+epoch_num = 50
 
 
-ds_train = xr.open_dataset('/pl/active/ATOC_SynopticMet/data/ar_data/Research3/Data/Combined_Data_CNN/train_trim.nc')
-ds_val = xr.open_dataset('/pl/active/ATOC_SynopticMet/data/ar_data/Research3/Data/Combined_Data_CNN/validate.nc')
+ds_train = xr.open_dataset('/pl/active/ATOC_SynopticMet/data/ar_data/Research3/Data/Combined_Daily_Data_CNN/train_full.nc')
+ds_val = xr.open_dataset('/pl/active/ATOC_SynopticMet/data/ar_data/Research3/Data/Combined_Daily_Data_CNN/validate_full.nc')
 
 train_random_shuffle = np.arange(len(ds_train.features))
 np.random.shuffle(train_random_shuffle )
 
 X_train = ds_train.features.values[train_random_shuffle]
-Y_train = ds_train.labels_1d.values[:,1:][train_random_shuffle] # don't include the no AR category
+Y_train = ds_train.labels_1d.values[train_random_shuffle] 
 time_train = ds_train.time[train_random_shuffle]
 
 val_random_shuffle = np.arange(len(ds_val.features))
 np.random.shuffle(val_random_shuffle)
 X_val = ds_val.features.values[val_random_shuffle]
-Y_val = ds_val.labels_1d.values[:,1:][val_random_shuffle] # don't include the no AR category
+Y_val = ds_val.labels_1d.values[val_random_shuffle]
 time_val = ds_val.time[val_random_shuffle]
 
 train_data = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
@@ -45,29 +45,28 @@ for batch in val_data:
 
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(20, (2, 2), padding='same', input_shape=(train_data.element_spec[0].shape[1:])),
+    tf.keras.layers.Conv2D(32, (3, 3), padding='same', input_shape=(train_data.element_spec[0].shape[1:]), activation ='relu'),
     tf.keras.layers.LeakyReLU(alpha=0.01),
     tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Conv2D(20, (2, 2), padding='same', input_shape=(train_data.element_spec[0].shape[1:])),
+    tf.keras.layers.Conv2D(32, (3, 3), padding='same', input_shape=(train_data.element_spec[0].shape[1:]), activation ='relu'),
     tf.keras.layers.LeakyReLU(alpha=0.01),
     tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Conv2D(50, (10, 10), padding='same', input_shape=(train_data.element_spec[0].shape[1:])),
+    tf.keras.layers.Conv2D(64, (3, 3), padding='same', input_shape=(train_data.element_spec[0].shape[1:]), activation ='relu'),
     tf.keras.layers.LeakyReLU(alpha=0.01),
     tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.GlobalAveragePooling2D(),
+
+    tf.keras.layers.Flatten(),
+    
 
     tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-    tf.keras.layers.Dense(100, activation='relu'),
-    tf.keras.layers.BatchNormalization(),
-
     tf.keras.layers.Dense(20,activation='sigmoid'),
 ])
 
 model.summary()
-model.compile(loss="mse",
-              optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), metrics=['accuracy'])
-history = model.fit(train_data,validation_data=val_data,epochs=epoch_num)
+callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta = .01,patience=5,  verbose = 1, )
+model.compile(loss=tf.keras.losses.BinaryFocalCrossentropy(gamma=2,from_logits=False),
+              optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4), metrics=['accuracy'])
+history = model.fit(train_data,validation_data=val_data,epochs=epoch_num, callbacks = [callback])
 history_pd = pd.DataFrame(history.history)
 y_preds_distribution = model.predict(val_data)
 
@@ -79,7 +78,7 @@ val_pd = pd.DataFrame(Y_val)
 val_pd = val_pd.set_index(np.array(time_val))
 
 
-new_folder = '/rc_scratch/reba1583/CNN_new3'
+new_folder = '/rc_scratch/reba1583/CNN_daily_test3_fulldata'
 os.system('mkdir '+new_folder)
 results_pd.to_csv(new_folder+'/results.csv')
 val_pd.to_csv(new_folder+'/val.csv')
